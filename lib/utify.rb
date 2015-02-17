60fe8f815beb2313032191fd682d844ca095729a
Felix Yadomi <dev.yadomi@gmail.com>
2015-02-17 20:45:47 +0100

    Splited functions, now work

--- lib/utify.rb
+++ lib/utify.rb
@@ -2,22 +2,28 @@ require "utify/version"
 
 require 'rspotify'
 require 'youtube_search'
-require 'ruby-youtube-dl'
 
 class Utify
   
   def searchFromFile(filepath)
-    File.readlines(filepath).each do |line|
-      clip =  YoutubeSearch.search(self.getTrack(line)).first['id']
-      cmd = `youtube-dl --no-progress --extract-audio --audio-format=mp3  --output="Test1.mp3" #{clip}`.strip
-      system(cmd)
+    tracks = IO.readlines(filepath)
+
+    tracks.each do | track |
+      title = self.get_track(track)
+      video_id = YoutubeSearch.search(title).first['video_id']
+      self.download_track(title, "https://www.youtube.com/watch?v=#{video_id}")
     end
   end
 
+  def get_track(uri)
+    id = uri.strip.split('/').last
+    track = RSpotify::Track.find(id)
+    "#{track.name} - #{track.artists[0].name}"
+  end
 
-  def getTrack(uri)
-    track = RSpotify::Track.find(uri.split('/').last)
-    "#{track.name} - {track.artists[0].name}"
+  def download_track(title, clip)
+    cmd = `youtube-dl --no-progress --extract-audio --audio-format=mp3  --output="#{title}.mp3" #{clip} > /dev/null`
+    system(cmd)
   end
 
 end
